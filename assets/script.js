c494cc676e9e79c88092ec68fb7ab6cffbeaae54
victorhenrie <victorhrdsilva@gmail.com>
2022-07-06 17:54:28 -0300

    adiciona tela de criar Quizz

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-let quizzData, optionsBoard, verifySelected, porcentageOfHits, questions
+let quizzData, optionsBoard, verifySelected, hitsPercentage, questions, levels, singleQuizz, id
 let totalClicks = 0
 let points = 0
 let linkQuizzes = 'https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes'
@@ -14,7 +14,7 @@ function loadQuizzList(quizz) {
     page.innerHTML = `
     <div class="create-quizz">
         <p>Você não criou nenhum quizz ainda :(</p>
-        <button>Criar Quizz</button>
+        <button onclick="createQuizz()">Criar Quizz</button>
     </div>
     <div class="all-quizzes">
         <h2>Todos os Quizzes</h2>
@@ -35,8 +35,8 @@ function loadQuizzList(quizz) {
 }
 
 function loadQuizzWithID(element) {
-    let id = element.classList[0]
-    let singleQuizz = axios.get(`${linkQuizzes}/${id}`)
+    id = element.classList[0]
+    singleQuizz = axios.get(`${linkQuizzes}/${id}`)
     singleQuizz.then(loadPageOfSingleQuizz)
 }
 
@@ -46,8 +46,8 @@ function comparator() {
 }
 
 function loadPageOfSingleQuizz (element) {
-    console.log(element)
     questions = element.data.questions
+    levels = element.data.levels
     page.innerHTML = `
     <div class="quizz-image" style="background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.57), rgba(0, 0, 0, 0.57)), url('${element.data.image}')">
         <p>${element.data.title}</p>
@@ -78,11 +78,11 @@ function loadPageOfSingleQuizz (element) {
             </div>`
         }
     }
+    document.querySelector('.quizz-image').scrollIntoView({block: "start", behavior: "smooth"});
 }
 
 function selectOption(element) {
     verifySelected = element.classList.contains('selected');
-    console.log(verifySelected)
     if (!verifySelected) {
         totalClicks++;
     }
@@ -109,17 +109,99 @@ function nextQuestion() {
     let next = document.querySelector('.pending')
     next.classList.remove('pending');
     next.querySelector('.pending')
-    next.scrollIntoView();
+    next.scrollIntoView({block: "start", behavior: "smooth"});
 }
 
 function verifyEndGame() {
     if (totalClicks == questions.length) {
-        porcentageOfHits = points/questions.length*100;
-        porcentageOfHits = Math.round(porcentageOfHits);
+        hitsPercentage = points/questions.length*100;
+        hitsPercentage = Math.round(hitsPercentage);
         showResult ()
     }
 }
 
 function showResult () {
+    let userLevel
+    for(let i = (levels.length - 1); i >= 0; i--) {
+        let minValueLevel = levels[i].minValue;
+        if (minValueLevel >= hitsPercentage){
+            userLevel = levels[i];
+        }
+    }
+    page.innerHTML += `
+    <div class="question-board result hidden">
+        <div class="question result" style="background-color: red;">
+            <p>${hitsPercentage}% de acerto: ${userLevel.title}</p>
+        </div>
+        <div class="result-box">
+            <img
+                src="${userLevel.image}">
+            <p>${userLevel.text}</p>
+        </div>
+    </div>
+    <div class="buttons">
+        <button onclick="resetQuizz()" class="reset">Reiniciar Quizz</button>
+        <button onclick="home()" class="home">Voltar para home</button>
+    </div>`
 
+    let result = document.querySelector('.result')
+    result.classList.remove('hidden')
+}
+
+function resetQuizz() {
+    singleQuizz = axios.get(`${linkQuizzes}/${id}`)
+    singleQuizz.then(loadPageOfSingleQuizz)
+    totalClicks = 0
+    hitsPercentage = 0
+    points = 0
+}
+
+function home() {
+    window.location.reload()
+}
+
+// CRIAR QUIZZ 
+
+let title, urlImage, questionNumber, questionNivel
+
+function createQuizz() {
+    page.classList.add('crate-quizz-information')
+    page.innerHTML = `
+    <div>
+        <h2>Comece pelo começo</h2>
+    </div>
+    <div class="crate-quizz-information input">
+        <input id="title" type="url" placeholder="Título do seu quizz">
+        <input id="url-image" type="text" placeholder="URL da imagem do seu quizz">
+        <input id="question-number" type="number" placeholder="Quantidade de perguntas do quizz">
+        <input id="question-nivel" type="number" placeholder="Quantidade de níveis do quizz">
+    </div>
+    <button onclick="sendInformationsQuestion()" class="create-button">Prosseguir pra criar perguntas</button>`
+}
+
+function sendInformationsQuestion(){
+    title = document.getElementById('title').value;
+    urlImage = document.getElementById('url-image').value;
+    questionNivel = document.getElementById('question-nivel').value;
+    questionNumber = document.getElementById('question-number').value;
+
+    function checkUrl(string) {
+        try {
+         let url = new URL(string);
+         return true;
+       } catch(err) {
+           return false;
+       }
+     }
+
+    checkUrl(urlImage)
+
+    if(title != undefined && urlImage != undefined && questionNumber != undefined && questionNivel != undefined) {
+        if (title.length > 20 && title.length < 65 && checkUrl(urlImage) && questionNumber >= 3 && questionNivel >= 2) {
+        } else {
+            alert('Preencha os campos corretamente para continuar')
+        }
+    } else {
+        alert('Preencha todos os campos para continuar')
+    }
 }
